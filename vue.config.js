1cc77e742c0819fab1f9f76232c5bc8317c3929f
Vladislav Iskakov <71043551+Sophrona@users.noreply.github.com>
2023-11-19 03:54:02 +0800

    fixed gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/medods-game/'  // Replace with your repository name
+  : '/',
   transpileDependencies: true
 })
